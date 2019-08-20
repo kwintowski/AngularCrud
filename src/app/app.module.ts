b9b526a1525b876a7bfd6bc57e331cad15219786
Andrew Kwintowski <andrew.kwintowski@gmail.com>
2019-08-20 21:02:09 +1000

    drop down lists and ngx date pickers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,8 +17,10 @@ import { CreateEmployeeComponent } from './employees/create-employee.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    BsDatepickerModule.forRoot(),
     AppRoutingModule,
-    FormsModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Department } from '../models/department.model';
 
 @Component({
   selector: 'app-create-employee',
@@ -7,13 +8,20 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
-
+  departments: Department[] = [
+    {id: 1, name: 'Help Desk'},
+    {id: 2, name: 'HR'},
+    {id: 3, name: 'IT'},
+    {id: 4, name: 'Payroll'},
+    {id: 5, name: 'Admin'}
+  ];
+  
   constructor() { }
 
   ngOnInit() {
   }
 
   saveEmployee(empForm: NgForm): void {
-    console.log(empForm);
+    console.log(empForm.value);
   }
 }
