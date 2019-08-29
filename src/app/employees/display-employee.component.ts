e6ac67475b9546a778f0d75aa5aedb50d1cc54a7
Andrew Kwintowski <andrew.kwintowski@gmail.com>
2019-08-29 21:30:41 +1000

    Using setters to display changes

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -6,27 +6,28 @@ import { Employee } from "../models/employee.model";
   templateUrl: "./display-employee.component.html",
   styleUrls: ["./display-employee.component.css"]
 })
-export class DisplayEmployeeComponent implements OnInit, OnChanges {
-  @Input() employeeId: number;
+export class DisplayEmployeeComponent implements OnInit {
+  
+  private _employeeId: number;
+
+  @Input()
+  set employeeId(val: number) {
+    console.log('employeeId changed from ' + JSON.stringify(this._employeeId) + ' to ' + JSON.stringify(val));
+    this._employeeId = val;
+  }
+  get employeeId(): number {
+    return this._employeeId;
+  };
 
   private _employee: Employee;
 
   @Input()
   set employee(val: Employee) {
+    console.log('employee changed from ' + JSON.stringify(this._employee) + ' to ' + JSON.stringify(val));
     this._employee = val;
   }
   get employee(): Employee {
     return this._employee;
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    for (const propName of Object.keys(changes)) {
-      const change = changes[propName];
-      const from = JSON.stringify(change.previousValue);
-      const to = JSON.stringify(change.currentValue);
-      
-      console.log(propName + ' from ' + from + ' to ' + to);
-    }
   }
 
   constructor() {}
