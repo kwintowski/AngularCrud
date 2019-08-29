376e168579222fce0e19fd59006acd89def739b3
Andrew Kwintowski <andrew.kwintowski@gmail.com>
2019-08-29 21:23:29 +1000

    loging changes with ngOnChanges

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,27 +1,35 @@
-import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
-import { Employee } from '../models/employee.model';
+import { Component, OnInit, Input, SimpleChanges, OnChanges } from "@angular/core";
+import { Employee } from "../models/employee.model";
 
 @Component({
-  selector: 'app-display-employee',
-  templateUrl: './display-employee.component.html',
-  styleUrls: ['./display-employee.component.css']
+  selector: "app-display-employee",
+  templateUrl: "./display-employee.component.html",
+  styleUrls: ["./display-employee.component.css"]
 })
-export class DisplayEmployeeComponent implements OnInit {
+export class DisplayEmployeeComponent implements OnInit, OnChanges {
+  @Input() employeeId: number;
+
   private _employee: Employee;
 
   @Input()
   set employee(val: Employee) {
-    console.log('Previous : ' + (this._employee ? this._employee.name : 'NULL'));
-    console.log('Current : ' + val.name);
     this._employee = val;
   }
   get employee(): Employee {
     return this._employee;
   }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    for (const propName of Object.keys(changes)) {
+      const change = changes[propName];
+      const from = JSON.stringify(change.previousValue);
+      const to = JSON.stringify(change.currentValue);
+      
+      console.log(propName + ' from ' + from + ' to ' + to);
+    }
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
